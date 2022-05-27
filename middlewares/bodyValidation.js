99cb6025f854fe463665aeea68da57f328e49a30
Victor Martins <victor.martins.duarte@gmail.com>
2022-05-27 18:52:53 -0300

    Fix: 95% of bodyValidation

--- middlewares/bodyValidation.js
+++ middlewares/bodyValidation.js
@@ -1,58 +1,44 @@
-const nameValidation = (name) => {
-  if (!name) {
-    console.log('sem nome');
-    return { message: 'O campo "name" é obrigatório' };
-  }
-  if (name.length < 3) {
-    console.log('nome menor que 3');
-    return { message: 'O "name" deve ter pelo menos 3 caracteres' };
-  }
-};
-
-const ageValidation = (age) => {
-  if (!age) {
-    return { message: 'O campo "age" é obrigatório' };
-  }
-  if (age < 18) {
-    return { message: 'A pessoa palestrante deve ser maior de idade' };
-  }
-};
-
 const invalidTalkMessage = { 
   message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
 };
+const regex = /^\d{2}\/\d{2}\/\d{4}$/;
+const messageName = { message: 'O "name" deve ter pelo menos 3 caracteres' };
+const messageAge = { message: 'A pessoa palestrante deve ser maior de idade' };
+const messageWatchedat = { message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' };
+const messageRate = { message: 'O campo "rate" deve ser um inteiro de 1 à 5' };
 
-const talkWatchedAtValidation = (watchedAt) => {
-  const regex = /^\d{2}\/\d{2}\/\d{4}$/;
+const isFilled = (name, age, talk) => {
+  const { watchedAt, rate } = talk;
+  if (!name) return { message: 'O campo "name" é obrigatório' };
+  if (!age) return { message: 'O campo "age" é obrigatório' };
   if (!watchedAt) return invalidTalkMessage;
-  if (!regex.test(watchedAt)) {
-    return { message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' };
-  }
-};
-
-const talkRateValidation = (rate) => {
   if (!rate) return invalidTalkMessage;
-  if (rate < 1 || rate > 5) {
-    return { message: 'O campo "rate" deve ser um inteiro de 1 à 5' };
-  }
 };
 
-const messages = (name, age, watchedAt, rate) => {
-  if (nameValidation(name)) nameValidation(name);
-  if (ageValidation(age)) ageValidation(age);
-  if (watchedAt) {
-    return talkWatchedAtValidation(watchedAt);
-  }
-  return talkRateValidation(rate);
+const rateValidation = (rate) => rate < 1 || rate > 5;
+const talkValidation = (name, age, { watchedAt, rate }) => {
+  if (name.length < 3) return messageName;
+  if (age < 18) return messageAge;
+  if (!regex.test(watchedAt)) return messageWatchedat;
+  if (rateValidation(rate)) return messageRate;
+  return false;
+};
+
+const isTalkValid = (talk) => {
+  const { watchedAt, rate } = talk;
+  if (!talk) return true;
+  if (talk === undefined) return true;
+  if (!watchedAt || !rate) return true;
 };
 
 const bodyValidation = (req, res, next) => {
   const { name, age, talk } = req.body;
-  const { watchedAt, rate } = talk;
-  if (!nameValidation(name) || !ageValidation(age)
-  || !talkWatchedAtValidation(watchedAt) || !talkRateValidation(rate)) {
-    res.status(400).json(messages(name, age, watchedAt, rate));
+  // const { watchedAt, rate } = talk;
+  console.log(talk);
+  if (!name || !age || isTalkValid(talk)) {
+    res.status(400).json(isFilled(name, age, talk));
   }
+  if (talkValidation(name, age, talk)) res.status(400).json(talkValidation(name, age, talk));
   next();
 };
 
