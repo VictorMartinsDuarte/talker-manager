16aa3e89363adb302969b55ee8d8b82774a0ad36
Victor Martins <victor.martins.duarte@gmail.com>
2022-05-26 22:25:50 -0300

    Ongoing feat: Req-05 Create endpoint POST /talker - Requisition auth and body keys validation

---
+++ middlewares/reqAuth.js
@@ -0,0 +1,13 @@
+const reqAuthentication = (req, res, next) => {
+  const { authorization } = req.headers;
+  if (!authorization) {
+    return res.status(401).json({ message: 'Token não encontrado' });
+  }
+  if (authorization.length !== 16) {
+    console.log(authorization);
+    return res.status(401).json({ message: 'Token inválido' });
+  }
+  next();
+};
+
+module.exports = reqAuthentication;
--- router.js
+++ router.js
@@ -3,6 +3,8 @@ const fs = require('fs').promises;
 const tokenGenerator = require('./utils/tokenGenerator');
 const emailValidation = require('./middlewares/emailValidation');
 const passwordValidation = require('./middlewares/passwordValidation');
+const reqAuthentication = require('./middlewares/reqAuth');
+const bodyValidation = require('./middlewares/bodyValidation');
 
 const router = express.Router();
 
@@ -37,6 +39,43 @@ router.get('/talker/:id', async (req, res, next) => {
 
 router.post('/login', emailValidation, passwordValidation, (_req, res) => {
   res.status(200).json({ token: tokenGenerator() });
+});
+
+router.post('/talker', reqAuthentication, bodyValidation, async (req, res, next) => {
+  try {
+    const { name, age, talk } = req.body;
+    const data = await fs.readFile(jsonData, 'utf-8');
+    const tklist = JSON.parse(data);
+    const id = tklist.length + 1;
+    const talker = { id, name, age, talk };
+    tklist.push(talker);
+    // Referência writeFile: https://www.geeksforgeeks.org/node-js-fs-writefile-method/
+    fs.writeFile(jsonData, JSON.stringify(tklist));
+    res.status(201).json(talker);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete('/talker/:id', async (req, res, next) => {
+  try {
+    const data = await fs.readFile(jsonData, 'utf-8');
+    const tklist = JSON.parse(data);
+    const { id } = req.params;
+    const tkIndex = tklist.indexOf((tk) => tk.id === id);
+    const newTklist = () => {
+      if (tkIndex >= 0) {
+        return tklist.splice(tkIndex, 1);
+      }
+      return res.status(400).send();
+    };
+    console.log(tkIndex);
+    // console.log(newTklist());
+    // fs.writeFile(jsonData, JSON.stringify(newTklist));
+    res.status(204).send();
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
