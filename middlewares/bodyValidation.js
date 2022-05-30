1b4fe2ff2c491e178a514492c45973973fb747e0
Victor Martins <victor.martins.duarte@gmail.com>
2022-05-30 19:09:42 -0300

    Fix: Req-05 and 06 Logic isTalkValid added

--- middlewares/bodyValidation.js
+++ middlewares/bodyValidation.js
@@ -7,13 +7,22 @@ const messageAge = { message: 'A pessoa palestrante deve ser maior de idade' };
 const messageWatchedat = { message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' };
 const messageRate = { message: 'O campo "rate" deve ser um inteiro de 1 à 5' };
 
+const isNameAndAgeValid = (name, age) => {
+  if (!name || !age) {
+    return true;
+  }
+};
+
+const isTalkValid = (talk) => {
+  if (!talk) return true;
+};
+
 const isFilled = (name, age, talk) => {
   const { watchedAt, rate } = talk;
-  if (!name) return { message: 'O campo "name" é obrigatório' };
-  if (!age) return { message: 'O campo "age" é obrigatório' };
+  if (!name) return ({ message: 'O campo "name" é obrigatório' });
+  if (!age) return ({ message: 'O campo "age" é obrigatório' });
   if (!watchedAt) return invalidTalkMessage;
   if (!rate) return invalidTalkMessage;
-  return invalidTalkMessage;
 };
 
 const rateValidation = (rate) => rate < 1 || rate > 5;
@@ -25,19 +34,7 @@ const talkValidation = (name, age, { watchedAt, rate }) => {
   return false;
 };
 
-// const isAllValid = (obj) => {
-//   if ((obj.talk in obj)) {
-//     // const { name, age, talk } = obj;
-//     // const { watchedAt, rate } = talk;
-//     return true;
-//   }
-//   // if (talk === undefined) return true;
-//   // if (!watchedAt || !rate) return true;
-//   return false;
-// };
-const isTalkValid = (obj) => {
-  const { talk } = obj;
-  // if (!(talk in obj)) return true;
+const watchedAtAndRateValidation = (talk) => {
   const { watchedAt, rate } = talk;
   if (watchedAt && rateValidation(rate)) return false;
   if (!watchedAt) return true;
@@ -47,12 +44,14 @@ const isTalkValid = (obj) => {
 
 const bodyValidation = (req, res, next) => {
   const { name, age, talk } = req.body;
-  // const { watchedAt, rate } = talk;
-  console.log(talk);
-  if (!name || !age || isTalkValid(req.body)) {
-    res.status(400).json(isFilled(name, age, talk));
+  const talkValid = isTalkValid(talk);
+  if (talkValid) return res.status(400).json(invalidTalkMessage);
+  if (isNameAndAgeValid(name, age) || watchedAtAndRateValidation(talk)) {
+    return res.status(400).json(isFilled(name, age, talk));
   }
-  if (talkValidation(name, age, talk)) res.status(400).json(talkValidation(name, age, talk));
+  if (talkValidation(name, age, req.body.talk)) { 
+    return res.status(400).json(talkValidation(name, age, req.body.talk));
+  }
   next();
 };
 
--- router.js
+++ router.js
@@ -26,8 +26,6 @@ router.get('/talker/search', reqAuth, async (req, res, next) => {
     // Referência req.query: https://pt.stackoverflow.com/questions/401582/a-diferen%C3%A7a-entre-req-params-e-req-query
     const { q } = req.query;
     const talkersFilter = tkList.filter(({ name }) => name.includes(q));
-    console.log(q);
-    console.log(talkersFilter);
     if (!talkersFilter) res.status(200).json([]);
     res.status(200).json(talkersFilter);
   } catch (error) {
@@ -79,15 +77,12 @@ router.put('/talker/:id', reqAuth, bodyValidation, async (req, res, next) => {
     const data = await fs.readFile(jsonData, 'utf-8');
     const tkList = JSON.parse(data);
     const id = +req.params.id;
-    const tkIndex = tkList.indexOf((tk) => tk.id === Number(id));
+    const tkIndex = tkList.findIndex((tk) => tk.id === Number(id));
     const modifiedTk = { id, name, age, talk };
-    console.log(tkIndex);
-    tkList.splice((tkIndex - 1), 1, modifiedTk);
+    tkList.splice(tkIndex, 1, modifiedTk);
     const newList = tkList;
-    console.log(newList);
     await fs.writeFile(jsonData, JSON.stringify(newList));
     res.status(200).json(modifiedTk);
-    // res.status(400).json({ message: 'Valores inválidos.' });
   } catch (error) {
     next(error);
   }
